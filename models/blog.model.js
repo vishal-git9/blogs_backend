8bf0e6f06c48f5bf28eef070780b8144c260f1a8
vishal-git9 <vishalsinghrajawat990@gmail.com>
2023-05-16 00:18:12 +0530

    able to populate the comments data as userdata

--- models/blog.model.js
+++ models/blog.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const blogsSchema = mongoose.Schema({
+const blogsSchema = new mongoose.Schema({
   username: String,
   title: String,
   content: String,
@@ -8,9 +8,23 @@ const blogsSchema = mongoose.Schema({
   date: String,
   likes: String,
   userId: String,
-  comments: [{ username: String, comment: String }],
+  comments: [{commentId:{type:mongoose.Schema.Types.ObjectId,ref:"commentsModel"}}]
+  // 64621a20b9d1853f5545e9e2
 });
+
+
+const commentsSchema = new mongoose.Schema({
+  userId:{ 		// this one populate
+    type: mongoose.Schema.Types.ObjectId,      
+    ref:"Userdb",
+},
+  comment: String,
+})
+
+const commentsModel = mongoose.model("commentsModel",commentsSchema)
+
+// const data = blogsModle.aggregate([{$lookup:{from:"Userdb",localfield:"username",foreignfiled:"username",as:"commeents"}}])
 
 const blogsModel = mongoose.model("blogsdb", blogsSchema);
 
-module.exports = blogsModel;
+module.exports = {blogsModel,commentsModel};
--- routes/blogs.routes.js
+++ routes/blogs.routes.js
@@ -22,10 +22,23 @@ blogRouter.get("/blogs",async(req,res)=>{
         }
     }
     try {
-        const blogData = await blogsModel.find(queryObj).sort({date:sortWise}).skip((Number(page)-1)*limit).limit(limit)
+        const blogData = await blogsModel.blogsModel.find(queryObj).populate({path:"comments.commentId",populate:"userId"}).sort({date:sortWise}).skip((Number(page)-1)*limit).limit(limit)
         res.status(200).send({msg:"here is all the blogs",blogData})
     } catch (error) {
         res.status(400).send({msg:"error while fetching blogs"})
+    }
+})
+
+
+// get user by username
+
+blogRouter.get("/blogs/comments",async(req,res)=>{
+    try {
+        const userData = await blogsModel.commentsModel.find().populate("userId")
+        console.log(userData)
+        res.status(200).send({msg:"data of the commented user",userData})
+    } catch (error) {
+        res.status(400).send({msg:"error while fetching the username"})
     }
 })
 
@@ -36,6 +49,34 @@ blogRouter.post("/blogs",blogMiddleware,async(req,res)=>{
         res.status(201).send({msg:"added a new blog"})
     } catch (error) {
         res.status(400).send({msg:"error while adding blog"})
+    }
+})
+
+
+blogRouter.post("/blogs/update/:id",async(req,res)=>{
+    const {id} = req.params
+    try {
+        const commentsData = new blogsModel.commentsModel({
+            userId:req.body.userId,
+            comment:req.body.comment
+        })
+        await commentsData.save()
+        const getDocument = await blogsModel.blogsModel.findById(id)
+        getDocument.comments.push({commentId:commentsData._id})
+        await getDocument.save()
+        res.status(201).send({msg:"added a new comment"})
+    } catch (error) {
+        res.status(400).send({msg:"error while adding comment"})
+    }
+})
+
+blogRouter.patch("/blogs/update/:id",blogMiddleware,async(req,res)=>{
+    const {id} = req.params
+    try {
+        const getDocument = await blogsModel.blogsModel.findByIdAndUpdate(id,req.body)
+        res.status(201).send({msg:"added a new like"})
+    } catch (error) {
+        res.status(400).send({msg:"error while adding like"})
     }
 })
 
